8704220f915faf46fac686a51c6dc414d112de22
Zac Collier <zacacollier@gmail.com>
2017-02-13 23:46:55 -0600

    add and style signUpSignIn

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,13 @@
+import request from 'superagent';
+
 export const REQUEST_GH_USER = 'REQUEST_GH_USER';
+// 52c32467d4c46675b4a32520bde09a27d485584d
+const API_URL = 'https://api.github.com/search/users?q=';
 
 export const requestGitHubUser = (term = null) => {
-  console.log(term);
+  const data = request.get(`${API_URL}${term.replace(/\s/g, '+')}`)
   return {
     type: REQUEST_GH_USER,
-    term
+    payload: data
   }
 }
--- src/containers/UserProfilePreview.js
+++ src/containers/UserProfilePreview.js
@@ -1,16 +1,28 @@
-import React                from 'react';
-import { Avatar }           from 'rebass';
+import React, { Component } from 'react';
+import { Avatar,
+         Block }            from 'rebass';
 
-const UserProfilePreview = () => {
+// thanks to artworkbean for the great svg - taken from the Noun Project
+class UserProfilePreview extends Component {
+  renderSignUpOrLogin = () => {
+    return (
+      <div><p>Sign up or Log in</p></div>
+    )
+  }
+  renderIsAuthenticated = () => {
+    <Avatar
+      circle
+      size={100}
+      src="http://lorempixel.com/64/64/cats"
+    />
+  }
+  render() {
     return (
       <div className='profile-preview'>
-        <Avatar
-          circle
-          size={100}
-          src="http://lorempixel.com/64/64/cats"
-        />
+       {this.renderSignUpOrLogin()}
       </div>
     )
+  }
 }
 
 export default UserProfilePreview;
